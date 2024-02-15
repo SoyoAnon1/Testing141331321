fe2c3d7d58d1e1d9f95707b4590a04abcaa2b673
SoyoAnon1 <146736782+SoyoAnon1@users.noreply.github.com>
2024-02-15 03:43:50 -0300

    Update code.py

--- VSColab/code.py
+++ VSColab/code.py
@@ -12,12 +12,12 @@ def Connect(password):
     subprocess.call('echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config', shell=True)
     print("username: root")
     print("password: ", password)
-    get_ipython().system_raw('/usr/sbin/sshd -D &')
-    subprocess.call('wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip',shell=True)
-    subprocess.call('unzip -qq -n ngrok-stable-linux-amd64.zip', shell=True)
+    #get_ipython().system_raw('/usr/sbin/sshd -D &')
+    subprocess.call('wget -q -c -nc https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz',shell=True)
+    subprocess.call('tar -xvzf ngrok-v3-stable-linux-amd64.tgz', shell=True)
     print("Get your authtoken from https://dashboard.ngrok.com/auth")
     authtoken = getpass.getpass()
-    get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
+    get_ipython().system_raw('./ngrok config add-authtoken $authtoken && ./ngrok tcp 22 &')
 
 def VSConfig(str):
     print("Host VSConfig")
